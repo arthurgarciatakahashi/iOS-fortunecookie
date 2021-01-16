5f3ec53dd72c07093c09462941ac7e4b9db1bae7
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-01-16 00:39:58 -0300

    ensure signup presenter show erros if no category is provider

--- PresentationTests/SignUpPresentationTests.swift
+++ PresentationTests/SignUpPresentationTests.swift
@@ -1,9 +1,50 @@
 import XCTest
 @testable import Presentation
 
-class PresentationTests: XCTestCase {
+class SignUpPresenter {
+    private let alertView: AlertView
+    
+    init(alertView: AlertView) {
+        self.alertView = alertView
+    }
+    
+    func signUp(viewModel: SignUpViewModel) {
+        if viewModel.category == nil || viewModel.category!.isEmpty {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Validation Failed", message: "Category is required"))
+        }
+    }
+}
 
-    func test_() throws {
+protocol AlertView {
+    func showMessage(viewModel: AlertViewModel)
+}
 
+struct AlertViewModel: Equatable{
+    var title: String
+    var message: String
+}
+
+struct SignUpViewModel {
+    var category: String?
+}
+
+class SignUpPresentationTests: XCTestCase {
+
+    func test_signup_should_show_error_message_if_category_is_not_provided() throws {
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignUpPresenter(alertView: alertViewSpy)
+        let signUpViewModel = SignUpViewModel(category: "any_category")
+        sut.signUp(viewModel: signUpViewModel)
+        XCTAssertEqual(alertViewSpy.viewModel, AlertViewModel(title: "Validation Failed", message: "Category is required"))
+    }
+}
+
+extension SignUpPresentationTests {
+    class AlertViewSpy: AlertView {
+        public var viewModel: AlertViewModel?
+        
+        func showMessage(viewModel: AlertViewModel) {
+            self.viewModel = viewModel
+        }
     }
 }
