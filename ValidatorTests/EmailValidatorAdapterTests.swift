3590595629367bfdc36352152a84a33a0920adc6
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-01-19 01:29:25 -0300

    move sut to a factory method

--- ValidatorTests/EmailValidatorAdapterTests.swift
+++ ValidatorTests/EmailValidatorAdapterTests.swift
@@ -14,12 +14,23 @@ public class EmailValidatorAdapter: EmailValidator {
 }
 class EmailValidatorAdapterTests: XCTestCase {
     func test_invalid_emails() throws {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSut()
         XCTAssertFalse(sut.isValid(email: ""))
         XCTAssertFalse(sut.isValid(email: "onlyletter@"))
         XCTAssertFalse(sut.isValid(email: "letters@letters"))
         XCTAssertFalse(sut.isValid(email: "letters@letters."))
         XCTAssertFalse(sut.isValid(email: "@letters.com"))
     }
+    func test_valid_emails() throws {
+        let sut = makeSut()
+        XCTAssertTrue(sut.isValid(email: "arthurgt@alumni.usp.br"))
+        XCTAssertTrue(sut.isValid(email: "arthurgt@usp.br@"))
+        XCTAssertTrue(sut.isValid(email: "arthurgt@test.com.br"))
+    }
+}
 
+extension EmailValidatorAdapterTests {
+    func makeSut() -> EmailValidatorAdapter {
+        return EmailValidatorAdapter()
+    }
 }
