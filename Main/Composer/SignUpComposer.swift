06bcb91a53c1fa98e02f41077635d5a95173f6b1
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-01-20 16:58:53 -0300

    remove controllerFactory

--- Main/Composer/SignUpComposer.swift
+++ Main/Composer/SignUpComposer.swift
@@ -1,9 +1,15 @@
 import Foundation
 import Domain
 import UI
+import Domain
+import Presentation
 
 public final class SignUpComposer {
     public static func composeControllerWith(getCookie: GetCookie) -> SignUpViewController {
-        return ControllerFactory.makeSignUp(getCookie: getCookie)
+        let controller = SignUpViewController.instanciate()
+        let presenter = SignUpPresenter(alertView: WeakVarProxy(controller), getCookie: getCookie, loadingView: WeakVarProxy(controller))
+        controller.signUp = presenter.signUp
+        
+        return controller
     }
 }
