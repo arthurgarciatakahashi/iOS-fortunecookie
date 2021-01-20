29713529e74dd4bc372cf4fe564e7cea1a6cb38f
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-01-20 18:13:58 -0300

    mode DispatchQueueDecorator to its own file

--- Main/Factories/UseCasesFactory.swift
+++ Main/Factories/UseCasesFactory.swift
@@ -15,30 +15,3 @@ public final class UseCaseFactory {
         return RemoteGetCookie(url: makeUrl(path: "all"), httpClient: httpClient)
     }
 }
-
-public final class MainQueueDispatchDecorator<T> {
-    private let instance: T
-    
-    public init(_ instance: T) {
-        self.instance = instance
-    }
-    
-    internal func dispatch(completion: @escaping ()-> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-        completion()
-    }
-}
-
-extension MainQueueDispatchDecorator: GetCookie where T: GetCookie {
-    public func get(completion: @escaping (Result<CookieModel, DomainError>) -> Void) {
-        instance.get() { [weak self] result in
-            self?.dispatch {
-                completion(result)
-            }
-        }
-    }
-}
-
-
