1d3fa131b965f110b89405ebd734dfb2322e858b
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-01-20 17:40:17 -0300

    rename testfile

--- MainTests/Composers/SignUpComposerTests.swift
+++ MainTests/Composers/SignUpComposerTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 import Main
 
-class SignUpIntegrationTests: XCTestCase {
+class SignUpComposerTests: XCTestCase {
 
     func test_ui_presentation_integration() throws {
         let sut = SignUpComposer.composeControllerWith(getCookie: GetCookieSpy())
