be022baf75dd52ff6df55d40877c4b9b44820e70
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-01-20 17:39:08 -0300

    remove unused code

--- MainTests/SignUpIntegrationTests.swift
+++ MainTests/SignUpIntegrationTests.swift
@@ -4,9 +4,6 @@ import Main
 class SignUpIntegrationTests: XCTestCase {
 
     func test_ui_presentation_integration() throws {
-        debugPrint("===============================")
-        debugPrint(Environment.variable(.apiBaseURl))
-        debugPrint("===============================")
         let sut = SignUpComposer.composeControllerWith(getCookie: GetCookieSpy())
         checkMemoryLeak(for: sut)
     }
