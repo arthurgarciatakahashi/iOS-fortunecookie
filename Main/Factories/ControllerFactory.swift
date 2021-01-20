06ad40a3b10db2510af37d646f9ede4d126f6969
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-01-20 16:51:59 -0300

    make weakvarproxy to avoid memory leak

--- Main/Factories/ControllerFactory.swift
+++ Main/Factories/ControllerFactory.swift
@@ -8,11 +8,29 @@ import Domain
 class ControllerFactory {
     static func makeSignUp(getCookie: GetCookie) -> SignUpViewController {
         let controller = SignUpViewController.instanciate()
-        let presenter = SignUpPresenter(alertView: controller, getCookie: getCookie, loadingView: controller)
+        let presenter = SignUpPresenter(alertView: WeakVarProxy(controller), getCookie: getCookie, loadingView: WeakVarProxy(controller))
         controller.signUp = presenter.signUp
         
         return controller
     }
 }
 
+class WeakVarProxy<T : AnyObject> {
+    private weak var instance: T?
+    
+    init(_ instance: T) {
+        self.instance = instance
+    }
+}
 
+extension WeakVarProxy: AlertView where T: AlertView {
+    func showMessage(viewModel: AlertViewModel) {
+        instance?.showMessage(viewModel: viewModel)
+    }
+}
+    
+extension WeakVarProxy: LoadingView where T: LoadingView {
+    func display(viewModel: LoadingViewModel) {
+        instance?.display(viewModel: viewModel)
+    }
+}
