0865a6311f3b5579d87be28609ee86121a73fb1a
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-02-04 20:21:03 -0300

    ensure remote authentication completes on main queue

--- MainTests/Factories/LoginControllerFactoryTests.swift
+++ MainTests/Factories/LoginControllerFactoryTests.swift
@@ -5,18 +5,18 @@ import Validation
 
 class LoginControllerFactoryTests: XCTestCase {
 
-//    func test_background_request_should_complete_on_main_thread() throws {
-//        let (sut, getCookieSpy) = makeSut()
-//        let exp = expectation(description: "waiting")
-//        sut.loadViewIfNeeded()
-//        sut.signUp?(makeSignUpViewModel())
-//        DispatchQueue.global().async {
-//            getCookieSpy.completeWithError(.unexpected)
-//            exp.fulfill()
-//        }
-//        checkMemoryLeak(for: sut)
-//        wait(for: [exp], timeout: 1)
-//    }
+    func test_login_background_request_should_complete_on_main_thread() throws {
+        let (sut, authenticationSpy) = makeSut()
+        let exp = expectation(description: "waiting")
+        sut.loadViewIfNeeded()
+        sut.login?(makeLoginViewModel())
+        DispatchQueue.global().async {
+            authenticationSpy.completeWithError(.unexpected)
+            exp.fulfill()
+        }
+        checkMemoryLeak(for: sut)
+        wait(for: [exp], timeout: 1)
+    }
     func test_login_compose_with_correct_validations() {
         let validation = makeLoginValidations()
         XCTAssertEqual(validation[0] as! RequiredFieldValidation, RequiredFieldValidation(fieldName: "email", fieldLabel: "Email"))
