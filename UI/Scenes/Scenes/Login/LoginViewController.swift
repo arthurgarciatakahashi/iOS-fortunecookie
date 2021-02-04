0c8077945a4731d98c8250d0fe7fa1bbba2118a6
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-02-04 00:25:30 -0300

    ensure loginviewcontroller starts hidden

---
+++ UI/Scenes/Scenes/Login/LoginViewController.swift
@@ -0,0 +1,26 @@
+import Foundation
+import UIKit
+import Presentation
+import Domain
+
+public final class LoginViewController: UIViewController, Storyboarded {
+    public var login: ((SignUpViewModel) -> Void)?
+
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var signIn: UIButton!
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    private func configure() {
+        title = "Login"
+        hideKeyboardOnTap()
+    }
+}
+   
