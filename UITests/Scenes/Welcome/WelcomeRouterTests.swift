90f3dd8270ea22f247077e48567324bcec57d592
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-02-09 00:28:26 -0300

    move welcomerouter to its own file

--- UITests/Scenes/Welcome/WelcomeRouterTests.swift
+++ UITests/Scenes/Welcome/WelcomeRouterTests.swift
@@ -43,22 +43,3 @@ extension WelcomeRouterTests {
     }
 }
 
-class WelcomeRouter {
-    private let nav: NavigationController
-    private let loginFactory: () -> LoginViewController
-    private let signUpFactory: () -> SignUpViewController
-    
-    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController, signUpFactory: @escaping () -> SignUpViewController) {
-        self.nav = nav
-        self.loginFactory = loginFactory
-        self.signUpFactory = signUpFactory
-    }
-    
-    public func goToLogin() {
-        nav.pushViewController(loginFactory(), animated: true)
-    }
-    
-    public func goToSignUp() {
-        nav.pushViewController(signUpFactory(), animated: true)
-    }
-}
