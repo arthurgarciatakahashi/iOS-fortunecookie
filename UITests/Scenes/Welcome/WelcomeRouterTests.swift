7741d326d960e0ca13a9b31ddf6da2ef622860dc
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2021-02-09 00:26:55 -0300

    ensure WelcomeRouter calls nav with signup factory on go to signup

--- UITests/Scenes/Welcome/WelcomeRouterTests.swift
+++ UITests/Scenes/Welcome/WelcomeRouterTests.swift
@@ -10,13 +10,21 @@ class WelcomeRouterTests: XCTestCase {
         XCTAssertEqual(nav.viewControllers.count, 1)
         XCTAssertTrue(nav.viewControllers[0] is LoginViewController)
     }
+    
+    func test_goToSignUp_calls_nav_with_correct_vc() {
+        let (sut, nav) = makeSut()
+        sut.goToSignUp()
+        XCTAssertEqual(nav.viewControllers.count, 1)
+        XCTAssertTrue(nav.viewControllers[0] is SignUpViewController)
+    }
 }
 
 extension WelcomeRouterTests {
     func makeSut() -> (sut: WelcomeRouter, nav: NavigationController) {
         let nav = NavigationController()
         let loginFactorySpy = LoginFactorySpy()
-        let sut = WelcomeRouter(nav: nav, loginFactory: loginFactorySpy.makeLogin)
+        let signUpFactorySpy = SignUpFactorySpy()
+        let sut = WelcomeRouter(nav: nav, loginFactory: loginFactorySpy.makeLogin, signUpFactory: signUpFactorySpy.makeSignUp)
         
         return (sut, nav)
     }
@@ -28,18 +36,29 @@ extension WelcomeRouterTests {
             return LoginViewController.instanciate()
         }
     }
+    class SignUpFactorySpy {
+        func makeSignUp() -> SignUpViewController {
+            return SignUpViewController.instanciate()
+        }
+    }
 }
 
 class WelcomeRouter {
     private let nav: NavigationController
     private let loginFactory: () -> LoginViewController
+    private let signUpFactory: () -> SignUpViewController
     
-    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController) {
+    public init(nav: NavigationController, loginFactory: @escaping () -> LoginViewController, signUpFactory: @escaping () -> SignUpViewController) {
         self.nav = nav
         self.loginFactory = loginFactory
+        self.signUpFactory = signUpFactory
     }
     
     public func goToLogin() {
         nav.pushViewController(loginFactory(), animated: true)
+    }
+    
+    public func goToSignUp() {
+        nav.pushViewController(signUpFactory(), animated: true)
     }
 }
